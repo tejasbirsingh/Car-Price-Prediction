018acd71299aa0c0b9aaa403ce853f37eba06eb1
tejasbirsingh <tejasbir.rekhi@gmail.com>
2020-12-23 10:39:39 +0530

    api added

--- app.py
+++ app.py
@@ -15,15 +15,20 @@ def Home():
     return render_template('index.html')
 
 
+@app.route('/api', methods=['GET'])
+def api():
+    return render_template('apiPage.html')
+
+
 standard_to = StandardScaler()
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predictapi", methods=['GET', 'POST'])
 
-def predict():
+def predictapi():
     Fuel_Type_Diesel = 0
     if request.method == 'POST':
-        Year = int(request.form['Year'])
+        Year = int(request.form['year'])
         Present_Price = float(request.form['Present_Price'])
         Kms_Driven = int(request.form['Kms_Driven'])
         Kms_Driven2 = np.log(Kms_Driven)
@@ -55,10 +60,51 @@ def predict():
         d={}
         d["output"]=str(output)
         return jsonify(d)
-        # if output < 0:
-        #     return render_template('index.html', prediction_texts="Sorry you cannot sell this car")
-        # else:
-        #     return render_template('index.html', prediction_text="You Can Sell The Car at {}".format(output))
+    
+    else:
+        return render_template('index.html')
+
+@app.route("/predict", methods=['GET', 'POST'])
+
+def predict():
+    Fuel_Type_Diesel = 0
+    if request.method == 'POST':
+        Year = int(request.form['year'])
+        Present_Price = float(request.form['Present_Price'])
+        Kms_Driven = int(request.form['Kms_Driven'])
+        Kms_Driven2 = np.log(Kms_Driven)
+        Owner = int(request.form['Owner'])
+        Fuel_Type_Petrol = request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol == 'Petrol'):
+            Fuel_Type_Petrol = 1
+            Fuel_Type_Diesel = 0
+        elif(Fuel_Type_Petrol == 'Diesel'):
+            Fuel_Type_Diesel = 1
+            Fuel_Type_Petrol = 0
+        else:
+            Fuel_Type_Petrol = 0
+            Fuel_Type_Diesel = 0
+        Year = 2020-Year
+        Seller_Type_Individual = request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual == 'Individual'):
+            Seller_Type_Individual = 1
+        else:
+            Seller_Type_Individual = 0
+        Transmission_Mannual = request.form['Transmission_Mannual']
+        if(Transmission_Mannual == 'Mannual'):
+            Transmission_Mannual = 1
+        else:
+            Transmission_Mannual = 0
+        prediction = model.predict([[Present_Price, Kms_Driven2, Owner, Year, Fuel_Type_Diesel,
+                                     Fuel_Type_Petrol, Seller_Type_Individual, Transmission_Mannual]])
+        output = round(prediction[0], 2)
+        # d={}
+        # d["output"]=str(output)
+        # return jsonify(d)
+        if output < 0:
+            return render_template('index.html', prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html', prediction_text="You Can Sell The Car at {}".format(output))
     else:
         return render_template('index.html')
 
